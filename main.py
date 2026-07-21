1358e4284608077eee44295d4678bd06eef1552f
Muhammad Awais Khalil <muhammadawaiskhalil3@gmail.com>
2026-07-21 12:32:59 +0500

    Stage 0 and 1: Create sql database and cretae data endpoints

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI, status, Body
 from fastapi.responses import JSONResponse
+import sqlite3
 
 app = FastAPI()
-#Stage 1
+
+def get_db():
+    conn = sqlite3.connect("tasks.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.on_event("startup")
+def setup_db():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS tasks (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   title TEXT NOT NULL,
+                   done BOOLEAN NOT NULL DEFAULT 0)
+                   """)
+    
+    cursor.execute("SELECT COUNT(*) FROM tasks")
+    count = cursor.fetchone()[0]
+
+    if count == 0:
+        cursor.execute("INSERT INTO tasks(title, done) VALUES ('Reading', 0)")
+        cursor.execute("INSERT INTO tasks(title, done) VALUES ('Coding', 1)")
+        cursor.execute("INSERT INTO tasks(title, done) VALUES ('Writing', 0)")
+    conn.commit()
+    cursor.close()
+
 
 @app.get('/')
 def api_desc():
@@ -20,10 +48,6 @@ tasks = [{"id":100, "title":"Reading","Done":False},
          {"id":101, "title":"Coding","Done":True},
          {"id":102, "title":"Writing","Done":False}]
 
-#Stage 2
-# @app.get('/tasks')
-# def get_all_tasks():
-#     return tasks
 
 @app.get('/tasks/{id}')
 def get_specific_task(id: int):
@@ -97,4 +121,3 @@ def delete_task(id:int):
         status_code=status.HTTP_404_NOT_FOUND,
         content={'error':f'Task {id} not found'}
     )
-
